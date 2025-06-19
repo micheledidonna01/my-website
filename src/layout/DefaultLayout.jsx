6b195885b7cecc694a95ecbc71bf18495a01d532
micheledidonna01 <micheledidonna337@gmail.com>
2025-06-19 15:39:07 +0200

    feat: create page structure

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
+
+const DefaultLayout = () => {
+    return <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
+}
+
+export default DefaultLayout;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+            <p>This is the main content of the homepage.</p>
+        </div>
+    );
+}
+
+export default Homepage;
